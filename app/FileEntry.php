bde7c14ea716e870e49f8b765f9864cd7762e252
Abrham Bas <abrham.bas01@gmail.com>
2019-12-14 19:43:18 +0800

    Trix attachments and some files here

---
+++ app/FileEntry.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FileEntry extends Model
+{
+
+	protected $fillable = ['filename', 'mime', 'path', 'size'];
+
+
+}
---
+++ attachments.js
@@ -0,0 +1,24 @@
+ function uploadFile(file, progressCallback, successCallback) {
+    var key = createStorageKey(file);
+    var formData = createFormData(key, file);
+    var xhr = new XMLHttpRequest()
+
+    xhr.open("POST", HOST, true)
+
+    xhr.upload.addEventListener("progress", function(event) {
+      var progress = event.loaded / event.total * 100
+      progressCallback(progress)
+    })
+
+    xhr.addEventListener("load", function(event) {
+      if (xhr.status == 204) {
+        var attributes = {
+          url: HOST + key,
+          href: HOST + key + "?content-disposition=attachment"
+        }
+        successCallback(attributes)
+      }
+    })
+    
+    xhr.send(formData)
+  }
